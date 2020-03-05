100c96d92786105afb1a82b4ab94328f0651bfdb
alpc91 <459388027@qq.com>
2020-03-05 11:54:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def parse_args():
 
     parser.add_argument('--iteration', type=int, default=300000, help='The number of training iterations')
     parser.add_argument('--batch_size', type=int, default=1, help='The size of batch size')
-    parser.add_argument('--print_freq', type=int, default=2, help='The number of image print freq')
+    parser.add_argument('--print_freq', type=int, default=1000, help='The number of image print freq')
     parser.add_argument('--save_freq', type=int, default=100000, help='The number of model save freq')
     parser.add_argument('--decay_flag', type=str2bool, default=True, help='The decay_flag')
 
